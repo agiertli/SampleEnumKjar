e7441acde02f7db63b66a70a1334f4687f493cac
anton <>
2019-04-08 15:01:34 +0200

    {/src/main/java/com/Product.java}

--- src/main/java/com/Product.java
+++ src/main/java/com/Product.java
@@ -6,12 +6,33 @@ package com;
 
 public class Product implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Product() {
-    }
+	private java.lang.String name;
+	private java.lang.String region;
 
+	public Product() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getRegion() {
+		return this.region;
+	}
+
+	public void setRegion(java.lang.String region) {
+		this.region = region;
+	}
+
+	public Product(java.lang.String name, java.lang.String region) {
+		this.name = name;
+		this.region = region;
+	}
 
 }
